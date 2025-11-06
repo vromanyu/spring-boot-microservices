badde345df566df5bc4e7bf384388be042ddcf43
Viktor Romanyuk <vromanyu@outlook.com>
2025-11-06 17:46:46 +0200

    add user entity
    * add user entity
    
    * helpful jpa properties

---
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/entity/User.java
@@ -0,0 +1,25 @@
+package com.vromanyu.ws.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}

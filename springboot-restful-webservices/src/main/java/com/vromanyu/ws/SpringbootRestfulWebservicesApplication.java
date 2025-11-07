f6c9109272aeb073b03038eae4db0ee100d9c99c
vromanyu <vromanyu@outlook.com>
2025-11-07 19:56:16 +0200

    Revert "will revert later"
    This reverts commit 38f10dddce2105c4d4df28686a08907a77473255.

--- springboot-restful-webservices/src/main/java/com/vromanyu/ws/SpringbootRestfulWebservicesApplication.java
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/SpringbootRestfulWebservicesApplication.java
@@ -8,7 +8,6 @@ public class SpringbootRestfulWebservicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
-        // comment to trigger ci
     }
 
 }

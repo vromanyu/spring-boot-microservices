745fa9064cf8e6048139207176a8981ecb52a854
vromanyu <vromanyu@outlook.com>
2025-11-09 02:34:48 +0200

    create UserService and UserServiceImpl
    - createUser

---
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/service/UserService.java
@@ -0,0 +1,9 @@
+package com.vromanyu.ws.service;
+
+import com.vromanyu.ws.entity.User;
+
+
+public interface UserService {
+    User createUser(User user);
+
+}
---
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.vromanyu.ws.service;
+
+import com.vromanyu.ws.entity.User;
+import com.vromanyu.ws.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public final class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}

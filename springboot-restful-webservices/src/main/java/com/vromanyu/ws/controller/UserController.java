7aec9f2823a8ab45a49e4ed77a0f91fdb31ae6c5
vromanyu <vromanyu@outlook.com>
2025-11-09 02:35:04 +0200

    implement UserController
    - createUser endpoint

---
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.vromanyu.ws.controller;
+
+import com.vromanyu.ws.entity.User;
+import com.vromanyu.ws.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<User> createUser(User user){
+        User savedUser = userService.createUser(user);
+        return ResponseEntity.
+                created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri())
+                .body(savedUser);
+    }
+
+}

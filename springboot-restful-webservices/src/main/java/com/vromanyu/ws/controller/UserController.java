4a3bbcfc21269c0a5a964b038997a00cbb4f68b1
Viktor Romanyuk <vromanyu@outlook.com>
2025-11-10 12:17:13 +0200

    document Users API [no ci]

--- springboot-restful-webservices/src/main/java/com/vromanyu/ws/controller/UserController.java
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/controller/UserController.java
@@ -2,7 +2,12 @@ package com.vromanyu.ws.controller;
 
 import com.vromanyu.ws.entity.User;
 import com.vromanyu.ws.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +22,23 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+            tags = {"users"},
+            method = "createUser",
+            operationId = "createUser",
+            requestBody =
+                    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                            description = "user",
+                            required = true,
+                            content = {
+                                    @Content(schema = @Schema(implementation = User.class))
+                            }
+                    )
+
+
+    )
     @PostMapping(value = "/users", consumes =  MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<User> createUser(User user){
         User savedUser = userService.createUser(user);
         return ResponseEntity.
@@ -25,23 +46,65 @@ public class UserController {
                 .body(savedUser);
     }
 
+
+    @Operation(
+            tags = {"users"},
+            method = "findUserById",
+            operationId = "findUserById",
+            parameters = {
+                    @Parameter(name = "id", required = true)
+            }
+
+    )
     @GetMapping(value = "/users/{id}", produces =  MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> findUserById(@PathVariable int id){
         User foundUser = userService.findUserById(id);
         return ResponseEntity.ok(foundUser);
     }
 
+    @Operation(
+            tags = {"users"},
+            method = "findAllUsers",
+            operationId = "findAllUsers"
+    )
     @GetMapping(value = "/users", produces =   MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<User>> findAllUsers(){
         return ResponseEntity.ok(userService.findAllUsers());
     }
 
+
+
+    @Operation(
+            tags = {"users"},
+            method = "updateUser",
+            operationId = "updateUser",
+            requestBody =
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "user",
+                    required = true,
+                    content = {
+                            @Content(schema = @Schema(implementation = User.class))
+                    }
+            )
+
+
+    )
     @PutMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> updateUser(@RequestBody User user){
         User savedUser = userService.updateUser(user);
         return ResponseEntity.ok(savedUser);
     }
 
+
+    @Operation(
+            tags = {"users"},
+            method = "deleteUser",
+            operationId = "deleteUser",
+            parameters = {
+                    @Parameter(name = "id", required = true)
+            }
+
+    )
     @DeleteMapping(value = "/users/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id){
         userService.deleteUser(id);

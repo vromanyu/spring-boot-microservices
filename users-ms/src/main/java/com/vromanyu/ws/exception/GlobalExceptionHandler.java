c18c31020b4412365e0da2ae40b0a0615fb5ea92
vromanyu <vromanyu@outlook.com>
2025-11-10 14:53:31 +0200

    handle MethodArgumentNotValidExceptions when validation fails

--- users-ms/src/main/java/com/vromanyu/ws/exception/GlobalExceptionHandler.java
+++ users-ms/src/main/java/com/vromanyu/ws/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.vromanyu.ws.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.OffsetDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -42,5 +45,16 @@ public class GlobalExceptionHandler {
                 "GENERIC_ERROR"
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest req) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                OffsetDateTime.now(),
+                e.getBindingResult().getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(", ")),
+                req.getRequestURI(),
+                "INVALID_ARGUMENT"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }

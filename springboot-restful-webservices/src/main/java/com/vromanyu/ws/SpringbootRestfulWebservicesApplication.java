ca3370130471dfd9914cbe0a2ecf642df7feeae4
vromanyu <vromanyu@outlook.com>
2025-11-10 12:14:38 +0200

    document Users API

--- springboot-restful-webservices/src/main/java/com/vromanyu/ws/SpringbootRestfulWebservicesApplication.java
+++ springboot-restful-webservices/src/main/java/com/vromanyu/ws/SpringbootRestfulWebservicesApplication.java
@@ -1,9 +1,17 @@
 package com.vromanyu.ws;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "UsersAPI",contact = @Contact(name = "Viktor Romanyuk", email = "viktor.r@gmail.com")),
+        tags = {@Tag(name = "users")}
+)
 public class SpringbootRestfulWebservicesApplication {
 
     public static void main(String[] args) {

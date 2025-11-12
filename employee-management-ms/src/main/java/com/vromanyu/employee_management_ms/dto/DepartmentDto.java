fa9abcfea2b5ec1b09be682ca0eb86f4c0260841
vromanyu <vromanyu@outlook.com>
2025-11-12 15:45:19 +0200

    update DepartmentDto to have employees

--- employee-management-ms/src/main/java/com/vromanyu/employee_management_ms/dto/DepartmentDto.java
+++ employee-management-ms/src/main/java/com/vromanyu/employee_management_ms/dto/DepartmentDto.java
@@ -2,13 +2,18 @@ package com.vromanyu.employee_management_ms.dto;
 
 import com.vromanyu.employee_management_ms.entity.Department;
 
+import java.util.List;
 
-public record DepartmentDto(Long id, String departmentName, String departmentDescription) {
+
+public record DepartmentDto(Long id, String departmentName, String departmentDescription, List<EmployeeDto> employees) {
 
     public static final class DepartmentMapper {
 
         public static DepartmentDto toDto(Department department) {
-            return new DepartmentDto(department.getId(), department.getDepartmentName(), department.getDepartmentDescription());
+            return new DepartmentDto(department.getId(),
+                    department.getDepartmentName(),
+                    department.getDepartmentDescription(),
+                    department.getEmployees().stream().map(EmployeeDto.EmployeeMapper::toDto).toList());
         }
 
         public static Department fromDto(DepartmentDto dto) {
@@ -16,6 +21,7 @@ public record DepartmentDto(Long id, String departmentName, String departmentDes
             department.setId(dto.id());
             department.setDepartmentName(dto.departmentName());
             department.setDepartmentDescription(dto.departmentDescription());
+            department.setEmployees(dto.employees().stream().map(EmployeeDto.EmployeeMapper::toEntity).toList());
             return department;
         }
 

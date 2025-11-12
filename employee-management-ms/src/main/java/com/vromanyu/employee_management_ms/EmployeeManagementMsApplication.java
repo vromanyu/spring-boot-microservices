3e81e22c96ed9523b75ad151f3da0ca243e68f2d
vromanyu <vromanyu@outlook.com>
2025-11-12 14:08:52 +0200

    initialize employee-management-ms

---
+++ employee-management-ms/src/main/java/com/vromanyu/employee_management_ms/EmployeeManagementMsApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.employee_management_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementMsApplication.class, args);
+    }
+
+}

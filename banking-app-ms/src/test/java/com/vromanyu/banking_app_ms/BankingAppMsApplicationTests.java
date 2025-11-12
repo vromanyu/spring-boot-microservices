5aebd6226635a82b30056e701696852b50fd6567
vromanyu <vromanyu@outlook.com>
2025-11-12 12:37:17 +0200

    initialize bank-app project

---
+++ banking-app-ms/src/test/java/com/vromanyu/banking_app_ms/BankingAppMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vromanyu.banking_app_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

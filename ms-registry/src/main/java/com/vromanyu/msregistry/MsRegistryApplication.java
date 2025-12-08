9b4b6838563e8fe656c918c17e92fc7a170cc7ad
vromanyu <vromanyu@outlook.com>
2025-12-08 17:04:10 +0200

    create ms-registry project

---
+++ ms-registry/src/main/java/com/vromanyu/msregistry/MsRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.msregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRegistryApplication.class, args);
+    }
+
+}

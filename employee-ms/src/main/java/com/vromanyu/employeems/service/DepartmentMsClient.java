c50af426ee6504fb4fd473a4332a1a77a57c51b1
vromanyu <vromanyu@outlook.com>
2025-12-08 17:45:28 +0200

    use service name instead of hardcoded url
    all services are registered with eureka. Eureka Server will choose where to send the request.

--- employee-ms/src/main/java/com/vromanyu/employeems/service/DepartmentMsClient.java
+++ employee-ms/src/main/java/com/vromanyu/employeems/service/DepartmentMsClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8181/api/departments", name = "department-ms")
+@FeignClient(name = "DEPARTMENT-MS")
 public interface DepartmentMsClient {
 
-    @GetMapping(value = "/{id}", produces = "application/json")
+    @GetMapping(value = "api/departments/{id}", produces = "application/json")
     DepartmentDto getById(@PathVariable(name = "id") Integer id);
 }

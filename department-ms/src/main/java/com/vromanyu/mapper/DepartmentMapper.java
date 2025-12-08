9d97cc58ff28d9d3e42c298a1b3dda4d2e39f44f
vromanyu <vromanyu@outlook.com>
2025-12-08 14:40:47 +0200

    create DepartmentMapper

---
+++ department-ms/src/main/java/com/vromanyu/mapper/DepartmentMapper.java
@@ -0,0 +1,25 @@
+package com.vromanyu.mapper;
+
+import com.vromanyu.departmentms.dto.DepartmentRequestDto;
+import com.vromanyu.departmentms.dto.DepartmentResponseDto;
+import com.vromanyu.departmentms.entity.Department;
+
+public class DepartmentMapper {
+
+    private DepartmentMapper() {}
+
+    public static Department toDepartment(DepartmentRequestDto departmentRequestDto) {
+        Department department = new Department();
+        department.setName(departmentRequestDto.name());
+        department.setDescription(departmentRequestDto.description());
+        department.setCode(departmentRequestDto.code());
+        return department;
+    }
+
+    public static DepartmentResponseDto toDepartmentResponseDto(Department department) {
+        return new DepartmentResponseDto(department.getId(),
+                department.getName(),
+                department.getDescription(),
+                department.getCode());
+    }
+}

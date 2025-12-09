ab24d253fc19fccec7281348bffcb2d32bbbc6b6
vromanyu <vromanyu@outlook.com>
2025-12-09 16:09:43 +0200

    routing with rewrites and custom headers appended to request/response

---
+++ api-gateway/src/main/java/com/vromanyu/apigateway/config/RoutingConfiguration.java
@@ -0,0 +1,32 @@
+package com.vromanyu.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutingConfiguration {
+
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/api-gateway/employees", "/api-gateway/employees/**")
+                        .filters(f -> {
+                            f.rewritePath("/api-gateway/employees(?<segment>.*)", "/api/employees${segment}");
+                            f.addRequestHeader("GATEWAY-REQUEST", "1");
+                            f.addResponseHeader("GATEWAY-RESPONSE", "1");
+                            return f;
+                        })
+                                .uri("lb://EMPLOYEE-MS"))
+                .route(p -> p.path("/api-gateway/departments", "/api-gateway/departments/**")
+                        .filters(f -> {
+                            f.rewritePath("/api-gateway/departments(?<segment>.*)", "/api/departments${segment}");
+                            f.addRequestHeader("GATEWAY-REQUEST", "1");
+                            f.addResponseHeader("GATEWAY-RESPONSE", "1");
+                            return f;
+                        })
+                        .uri("lb://DEPARTMENT-MS")).build();
+    }
+}

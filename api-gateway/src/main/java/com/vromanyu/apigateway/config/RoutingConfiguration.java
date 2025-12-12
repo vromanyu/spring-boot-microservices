78be14bec70e64fb608ebacfbd0754eaed42520d
Viktor Romanyuk <vromanyu@outlook.com>
2025-12-12 17:00:36 +0200

    retry for departments-ms
    retry max 3 times for all HTTP GET methods with backoff

--- api-gateway/src/main/java/com/vromanyu/apigateway/config/RoutingConfiguration.java
+++ api-gateway/src/main/java/com/vromanyu/apigateway/config/RoutingConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+import java.time.Duration;
 
 @Configuration
 public class RoutingConfiguration {
@@ -35,6 +38,11 @@ public class RoutingConfiguration {
                             f.rewritePath("/api-gateway/departments(?<segment>.*)", "/api/departments${segment}");
                             f.addRequestHeader("GATEWAY-REQUEST", "1");
                             f.addResponseHeader("GATEWAY-RESPONSE", "1");
+                            f.retry(config -> {
+                                config.setRetries(3);
+                                config.setMethods(HttpMethod.GET);
+                                config.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true);
+                            });
                             return f;
                         })
                         .uri("lb://DEPARTMENT-MS")).build();

ffca7fb56187300f892c9b8fc110ad0c962f5f6d
vromanyu <vromanyu@outlook.com>
2025-12-12 16:09:32 +0200

    provide fallback mechanism in openfeign client

--- employee-ms/src/main/java/com/vromanyu/employeems/service/DepartmentMsClient.java
+++ employee-ms/src/main/java/com/vromanyu/employeems/service/DepartmentMsClient.java
@@ -2,12 +2,26 @@ package com.vromanyu.employeems.service;
 
 import com.vromanyu.employeems.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "DEPARTMENT-MS")
+@FeignClient(name = "DEPARTMENT-MS", fallback = DepartmentMsClient.DepartmentMsClientFallback.class)
 public interface DepartmentMsClient {
 
     @GetMapping(value = "api/departments/{id}", produces = "application/json")
     DepartmentDto getById(@PathVariable Integer id);
+
+    @Component
+    class DepartmentMsClientFallback implements DepartmentMsClient {
+
+        @Override
+        public DepartmentDto getById(Integer id) {
+            return new DepartmentDto(0,
+                    "fallback department",
+                    "a fallback department in case the actaul department couldn't be fetched",
+                    "000"
+            );
+        }
+    }
 }

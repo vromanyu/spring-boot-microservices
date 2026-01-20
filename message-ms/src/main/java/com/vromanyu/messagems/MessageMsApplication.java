69ec1e1fe4d4cc32a96fa778aae13b719da5a3bc
vromanyu <vromanyu@outlook.com>
2026-01-20 20:57:02 +0200

    refactors

--- message-ms/src/main/java/com/vromanyu/messagems/MessageMsApplication.java
+++ message-ms/src/main/java/com/vromanyu/messagems/MessageMsApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;

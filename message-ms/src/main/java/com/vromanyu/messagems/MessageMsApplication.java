6f76e94ad5b62ad060028884dfd4f2009cbbcae4
vromanyu <vromanyu@outlook.com>
2026-01-20 12:06:34 +0200

    initialize message-ms microservice leveraging Spring Cloud Function

---
+++ message-ms/src/main/java/com/vromanyu/messagems/MessageMsApplication.java
@@ -0,0 +1,13 @@
+package com.vromanyu.messagems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageMsApplication.class, args);
+    }
+
+}
---
+++ message-ms/src/test/java/com/vromanyu/messagems/MessageMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vromanyu.messagems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
